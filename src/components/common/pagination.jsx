55fa28e1b9b9cb90bc77729b8477a33d6d4368ae
Rogério Martins <rogeriosvaldo@gmail.com>
2022-01-07 18:32:58 +0000

    TypeChecking  with PropTypes

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from "prop-types"
 import _ from "lodash";
-import { isNullishCoalesce } from 'typescript';
+
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange} = props;
@@ -26,7 +27,14 @@ const Pagination = (props) => {
   </nav>
 
 }
- 
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired, 
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+}
+
+
 export default Pagination;
 
 
