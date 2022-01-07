0aad8851863221bd54833b98c903432371293f8e
Rogério Martins <rogeriosvaldo@gmail.com>
2022-01-07 18:22:46 +0000

    Update pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
-
+import { paginate } from "../utils/paginate";
 
 class Movies extends React.Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
     pageSize: 4
   }
   handleDelete = (movie) => {   
@@ -23,12 +24,16 @@ class Movies extends React.Component {
     this.setState({movies})
   };
   handlePageChange = page => {
-    console.log(page)
+    this.setState({ currentPage: page})
   }
   render() {
-    const { length: count } = this.state.movies; 
-    if(count === 0) 
-    return <p>There are no movies in the database.</p>;
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state; 
+    if(count === 0) return <p>There are no movies in the database.</p>;
+    
+
+    const movies = paginate(allMovies, currentPage, pageSize)
+
     return (
       <React.Fragment>
         <p>Showing {count} movies in the database</p><table className="table">
@@ -43,7 +48,7 @@ class Movies extends React.Component {
         </tr>
       </thead>
       <tbody>
-        { this.state.movies.map(movie=>(
+        { movies.map(movie=>(
             <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
@@ -57,9 +62,11 @@ class Movies extends React.Component {
       </tbody>
     </table>
       <Pagination 
-      itemsCount={count} 
-      pageSize={this.state.pageSize} 
-      onPageChange={this.handlePageChange}/>
+        itemsCount={count} 
+        pageSize={pageSize}
+        currentPage={currentPage} 
+        onPageChange={this.handlePageChange}
+      />
       </React.Fragment>
     )
   }
