3ce7459126cc7d3f79b7e4682b123c959dc58794
Rogério Martins <rogeriosvaldo@gmail.com>
2022-01-08 20:22:28 +0000

    Adding sort icon

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -1,3 +1,4 @@
+import { inArray } from 'jquery';
 import React, { Component } from 'react';
 
 
@@ -14,11 +15,23 @@ class TableHeader extends React.Component {
   }
   this.props.onSort(sortColumn);
   }
+
+  renderSortIcon = column => {
+    const { sortColumn}  = this.props;
+
+    if(column.path !== sortColumn.path) return null;
+    if(sortColumn.order === 'asc') return <i className="fa fa-sort-asc"></i>
+
+    return <i className="fa fa-sort-desc"></i>
+
+  }
   render() { 
     return <thead>
       <tr>
         { this.props.columns.map(column => (
-          <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{ column.label }</th>
+          <th classeName="clickable" key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{ column.label } {this.renderSortIcon(column)}
+          
+          </th>
         ))}
       </tr>
     </thead>
